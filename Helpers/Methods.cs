9b78da98bea7da12c6c39df7dae984bf5b920374
PKCPH <patrick@klingaa.dk>
2022-08-19 17:54:06 +0200

    delayed push, core of game mechanics, need of filepath to be fixed and implement other games.

---
+++ Helpers/Methods.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordleGame.Helpers
+{
+    public class WordleHelpers
+    {
+
+        public void Game()
+        {
+            string hiddenWord = GetRandomWord();
+            string? guess = null;
+
+            for (int counter = 0; counter < 5; counter++)
+            {
+                guess = Console.ReadLine();
+                WordChecker(guess, hiddenWord, counter);
+            }
+        }
+
+        public static void WordChecker(string guess, string hiddenWord, int counter)
+        {
+            Console.CursorLeft = 0;
+            //if word is 5 letters
+            if (guess.Length != 5) Console.WriteLine("Word is not 5 letter");
+            if (!Regex.IsMatch(guess, @"^[a-zA-Z]+$")) Console.WriteLine("Word is not letters from A-Z");
+            if (guess == hiddenWord) Console.WriteLine("correct");
+            else
+            {
+                for (int i = 0; i < hiddenWord.Length; i++)
+                {
+                    if (hiddenWord[i] == guess[i])
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write(guess[i]);
+                    }
+                    if (hiddenWord.Contains(guess[i]))
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.Write(guess[i]);
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Write(guess[i]);
+                    }
+                }
+            }
+
+
+            Console.ResetColor();
+
+        }
+
+
+
+
+        public static string GetRandomWord()
+        {
+            System.Diagnostics.Debugger.Launch();
+            var path = GetPathToList();
+            ////path to textFile
+            //var path = Path.Combine(Directory.GetCurrentDirectory(), @"WordLists\WordListEnglish.txt");
+
+            var lines = File.ReadAllLines(path);
+
+            var r = new Random();
+            //random number
+            var randomLineNumber = r.Next(0, lines.Length - 1);
+
+            var line = lines[randomLineNumber];
+
+            return line;
+        }
+
+
+        public static string GetPathToList()
+        {
+            //current directory passed codecase to getdirectoryname to get info about outputdirectory
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+
+            //combining pathg with short relative path of textfile path
+            var iconPath = Path.Combine(outPutDirectory, "C:\\Users\\PatrickKlingaa\\source\\repos\\WordleGame\\Helpers\\listofwords.txt");
+
+            //converting new path to relativer path string with the Uri.LocalPath property.
+            string icon_path = new Uri(iconPath).LocalPath;
+
+            return iconPath;
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WordleGame.Helpers;
+
+namespace WordleGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the word: ");
+
+            WordleHelpers wordleGame = new WordleHelpers();
+            wordleGame.Game();
+
+            Console.ReadKey();
+        }
+    }
+
+}
