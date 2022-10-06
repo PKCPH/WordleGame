72673f07c50f065b47db988495c44d2fc1afd2cc
PKCPH <patrick@klingaa.dk>
2022-10-06 18:36:56 +0200

    removed menu.

--- Program.cs
+++ Program.cs
@@ -8,29 +8,12 @@ namespace WordleGame
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Choose a game to play1: \n 1. Wordle, 2. WordSmith 3. Exit Application (not a game lol...)");
-            bool menuIsTrue = true;
-
-            while (menuIsTrue)
+            while (true)
             {
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
                         Wordle wordle = new();
                         wordle.Game();
-                        break;
-                    case "2":
-                        WordSmith wordSmith = new();
-                        wordSmith.Game();
-                        break;
-                    case "3":
-                        menuIsTrue = false;
-                        break;
-                    default:
-                        menuIsTrue = true;
-                        break;
-                }
+                 
+                
             }
         }
     }
